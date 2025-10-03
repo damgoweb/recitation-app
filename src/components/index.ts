386302f7e5241c72731722ca1452f7040f6d1587
damgoweb <dannojo.engineer@gmail.com>
2025-10-03 08:24:08 +0800

    feat: implement text creation form with validation

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,4 +2,5 @@ export { Header } from './Header';
 export { ProgressBar } from './ProgressBar';
 export { TextCard } from './TextCard';
 export { TextList } from './TextList';
+export { TextForm } from './TextForm';
 export * from './ui';
